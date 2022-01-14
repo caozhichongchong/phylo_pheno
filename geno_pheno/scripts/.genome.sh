8a57aa632470cd8b5b8103234e5b5df76fb761ef
caozhichongchong <caozhichongchong@gmail.com>
2022-01-14 15:34:34 -0500

    example dataset

---
+++ geno_pheno/scripts/.genome.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source ~/.bashrc
+diamond blastp --query geno_pheno/genomes/4.faa --db geno_pheno/database/Butyrate.pro.fasta.dmnd --out geno_pheno/genomes/4.faa.diamond.txt --outfmt 6 --max-target-seqs 2 --evalue 1e-2 --threads 4
---
+++ geno_pheno/submitalljob.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source ~/.bashrc
+jobmit geno_pheno/scripts/1.faa.sh
+jobmit geno_pheno/scripts/2.faa.sh
+jobmit geno_pheno/scripts/3.faa.sh
+jobmit geno_pheno/scripts/4.faa.sh
